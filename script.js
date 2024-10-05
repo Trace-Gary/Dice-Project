9a9cd250c257eed6ac6b73ddeb5b85ee8200acb5
Trace-Gary <tracegary@gmail.com>
2024-10-04 20:51:19 -0500

    this is for dice-logic

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function rollDice() {
-    let sides = document.getElementById('numSides').Value
-    let result = getDiceResult(sides)
-    document.getElementById('dice-result').innerText = 'you rolled a '+reslult
+    let sides = document.getElementById ('numSides'). value
+    let result = getDiceResult (sides)
+    document.getElementById('dice-result').innerText = 'you rolled a '+result
 }
-
-function getDiceResult(max) {
-    let result = Math.ceil(Math.random() * max)
+    function getDiceResult(max) {
+    let result = Math.ceil (Math. random () * max)
     return result
-}
-
-for (let i = 1; i< 100; i++) {
-    let testResult = getDiceResult(i)
-    if (testResult > i || testResult <1) throw Error("Invalid dice result "+testResult+" for max " +i)
-}
+    }
+    for (let i= 1; i< 100;i++) {
+        let testResult = getDiceResult(i)
+        if (testResult > i || testResult <1) throw Error ("invalid dice result" +testResult+"for max" +i )
+     }
